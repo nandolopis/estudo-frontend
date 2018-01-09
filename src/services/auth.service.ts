2458038aeb5277582c5a1929f3b49fe8fb2e397e
Fernando Lopes <nandolopis@gmail.com>
2018-01-09 12:50:37 -0200

    Fazendo o app aproveitar o usuario logado na tela inicial

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -6,7 +6,6 @@ import { LocalUser } from "../models/local_user";
 import { StorageService } from "./storage.service";
 import { JwtHelper } from 'angular2-jwt';
 
-
 @Injectable()
 export class AuthService {
 
@@ -23,7 +22,15 @@ export class AuthService {
                 observe: 'response',
                 responseType: 'text'
             });
-
+        }
+        refreshToken(){
+            return this.http.post(
+                `${API_CONFIG.baseUrl}/auth/refresh_token`,
+                {},
+                {
+                    observe: 'response',
+                    responseType: 'text'
+                });
         }
 
             successfulLogin(authorizationValue : string) {
